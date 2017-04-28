f8cef94ce87ff298b5c08c0964a36df7e4901b2e
rallen0150 <rallen0150@gmail.com>
2017-04-28 11:19:30 -0400

    starting my draftcast, but putting it on a django project

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from bs4 import BeautifulSoup
+import time
+import requests
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
