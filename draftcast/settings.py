3f5f9980902ee73fbca3c5b3db4545179ff5208a
rallen0150 <rallen0150@gmail.com>
2017-04-29 10:45:16 -0400

    deploying on heroku

--- draftcast/settings.py
+++ draftcast/settings.py
@@ -70,8 +70,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'draftcast.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': {
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
